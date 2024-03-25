5842ab19ffcbe2d0cd4f3b7ba7cc5a0eb0b4754e
spaceyjase <429978+spaceyjase@users.noreply.github.com>
2024-03-25 17:32:45 +0000

    feat: selector (or) node

---
+++ BehaviourTree/Selector/Selector.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using BehaviourTree.Node;
+
+namespace BehaviourTree.Selector;
+
+public class Selector : Node.Node
+{
+    public Selector() { }
+
+    public Selector(IEnumerable<Node.Node> children)
+        : base(children) { }
+
+    public override NodeState Evaluate()
+    {
+        foreach (Node.Node node in this.Children)
+        {
+            switch (node.Evaluate())
+            {
+                case NodeState.Failure:
+                    continue;
+                case NodeState.Success:
+                    this.state = NodeState.Success;
+                    return this.state;
+                case NodeState.Running:
+                    this.state = NodeState.Running;
+                    return this.state;
+                default:
+                    continue;
+            }
+        }
+        this.state = NodeState.Failure;
+        return this.state;
+    }
+}
